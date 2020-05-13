1cc901dd845c99c7f189981ec6c4b98383ad07db
jorgeml8 <jorge.malopez@gmail.com>
2020-05-13 10:50:14 -0500

    Actualizacion de main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,142 +1,75 @@
+//Create a single global variable
 var MAPAPP = {};
 MAPAPP.markers = [];
 MAPAPP.currentInfoWindow;
 MAPAPP.pathName = window.location.pathname;
 
 $(document).ready(function() {
-    initialize(MAPAPP.pathName);
-    //populateMarkers(MAPAPP.pathName);
+    initialize();
+    populateMarkers(MAPAPP.pathName);
 });
 
-
-function FontAwesomeMarker(latlng, map, args) {
-	this.latlng = latlng;	
-	this.args = args;	
-	this.setMap(map);	
-}
-
-FontAwesomeMarker.prototype = new google.maps.OverlayView();
-
-FontAwesomeMarker.prototype.draw = function() {
-	var self = this,
-  	panes = this.getPanes(),
-  	marker = this.marker;
-	
-	if (!marker) {
-	  marker = this.marker = document.createElement('div');
-		marker.className = 'marker';
-		
-    var icon = document.createElement('i');
-    icon.className = 'fa fa-' + this.args.icon;
-    icon.style.fontSize = this.args.fontSize;
-    icon.style.color = this.args.color;
-    marker.appendChild(icon);
-    
-    var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
-    if (point) {
-      marker.style.left = (point.x - 25) + 'px';
-      marker.style.top = (point.y - 25) + 'px';
-    }
-    
-		google.maps.event.addDomListener(marker, "click", function(event) {
-			alert('You clicked on a custom marker!');			
-			google.maps.event.trigger(self, "click");
-		});
-
-		panes.overlayImage.appendChild(marker);
-	}
-};
-
-FontAwesomeMarker.prototype.remove = function() {
-	if (this.marker) {
-		this.marker.parentNode.removeChild(this.marker);
-		this.marker = null;
-	}	
-};
-
-FontAwesomeMarker.prototype.getPosition = function() {
-	return this.latlng;	
-};
-
-function initialize(dataType) {
-	var myLatlng = new google.maps.LatLng(-33.9,151.2),
-    mapOptions = {
-      zoom: 15,
-      center: myLatlng,
-      disableDefaultUI: true
+//Initialize our Google Map
+function initialize() {
+    var center = new google.maps.LatLng(39.9543926,-75.1627432);
+    var mapOptions = {
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: center,
     };
-	
-  var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-  
+    this.map = new google.maps.Map(document.getElementById('map_canvas'),
+        mapOptions);
+};
 
+// Fill map with markers
+function populateMarkers(dataType) {
 
-  apiLoc = typeof apiLoc !== 'undefined' ? apiLoc : '/data/' + dataType + '.json';
-  $.getJSON(apiLoc, function(data) {
-    //For each item in our JSON, add a new map marker
-    $.each(data, function(i, ob) {
-        var marker = new google.maps.Marker({
-            //map: map,
-            latLan: new google.maps.LatLng(this.location.coordinates[0], this.location.coordinates[1]),
-            shopname: this.shopname,
-            details: this.details,
-            website: this.website,
-            icon: this.icon,
-            color: this.color,
-            fontSize: '35px'
-        });
-
-  
-
-        MAPAPP.markers.push(marker);
+    
+    apiLoc = typeof apiLoc !== 'undefined' ? apiLoc : '/data/' + dataType + '.json';
+    // jQuery AJAX call for JSON
+    $.getJSON(apiLoc, function(data) {
+        //For each item in our JSON, add a new map marker
+        $.each(data, function(i, ob) {
+            var marker = new google.maps.Marker({
+                map: map,
+                position: new google.maps.LatLng(this.location.coordinates[0], this.location.coordinates[1]),
+                shopname: this.shopname,
+                details: this.details,
+                website: this.website,
+                icon: {
+                    path: google.maps.SymbolPath.CIRCLE,
+                    fillColor: '#F6F6F6',
+                    fillOpacity: .5,
+                    strokeWeight: 1.5,
+                    strokeColor: '#1E8E7D',
+                    scale: 25
+                  },
+                label: {
+                    fontFamily: "FontAwesome",
+                    fontWeight: '900',
+                    text: this.icon,
+                    color: this.color,
+                    fontSize: '25px'
+                  }
+                  
+            });
+       
+    	//Build the content for InfoWindow
+            var content = '<h1 class="mt0"><a href="' + marker.website + '" target="_blank" title="' + marker.shopname + '">' + marker.shopname + '</a></h1><p>' + marker.details + '</p>';
+            
+            marker.infowindow = new google.maps.InfoWindow({
+            	content: content,
+            	maxWidth: 400
+            });
+    	//Add InfoWindow
+            google.maps.event.addListener(marker, 'click', function() {
+                if (MAPAPP.currentInfoWindow) MAPAPP.currentInfoWindow.close();
+                marker.infowindow.open(map, marker);
+                MAPAPP.currentInfoWindow = marker.infowindow;
+            });
         
-      });
+            MAPAPP.markers.push(marker);
+        });
     });
-/*
-  var markers = [
-  	{
-      latLan: new google.maps.LatLng(-33.9,151.2),
-      icon: 'cutlery',
-      color: '#346698',
-      fontSize: '35px'
-    },
-    {
-     	latLan: new google.maps.LatLng(-33.8939,151.207333),
-      icon: 'anchor',
-      color: '#B90C13',
-      fontSize: '35px'
-    },
-    {
-      latLan: new google.maps.LatLng(-33.895,151.195),
-      icon: 'automobile',
-      color: '#39A00F',
-      fontSize: '35px'
-    },
-    {
-      latLan: new google.maps.LatLng(-33.905,151.195),
-      icon: 'headphones',
-      color: '#000',
-      fontSize: '35px'
-    },
-     {
-     	latLan: new google.maps.LatLng(-33.9039,151.207333),
-      icon: 'child',
-      color: '#26C2C3',
-      fontSize: '35px'
-    },
-  ]*/
-  
-  MAPAPP.markers.forEach(function(item) {
-    new FontAwesomeMarker(
-      item.latLan, 
-      map,
-      {
-        icon: item.icon,
-        color: item.color,
-        fontSize: item.fontSize
-      }
-    );
-  });
-  
-}
+};
 
-google.maps.event.addDomListener(window, 'load', initialize);
